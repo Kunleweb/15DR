beb0bcf6185e0c4ab0b508f865fa8af5812ef80f
unknown <aadeye302@caledonian.ac.uk>
2025-10-08 22:04:34 +0100

    react

--- my-react/src/main.jsx
+++ my-react/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from "react-dom"
 import './index.css'
-import App from './App.jsx'
+import './App.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ <h1 className='read-the-docs' >Welcome to react page</h1>
 )
+
+
+// ReactDOM.render(<h1>Welcome to react page</h1>, document.getElementById("root"))
+
